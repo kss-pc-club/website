55719201962b4ea67c1d885cd42bc96cec37efac
Ryoga.exe <ryoga.dev@gmail.com>
2021-06-17 20:25:04 +0900

    Update: fontawesome関連のプラグインを追加

--- .gatsby/gatsby-config.ts
+++ .gatsby/gatsby-config.ts
@@ -31,6 +31,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-lodash`,
+    `gatsby-plugin-fontawesome-css`,
     /*
     {
       resolve: `gatsby-source-filesystem`,
