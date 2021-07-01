e006c5deff342ef38deba8adb46e0ddd61167c26
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-07-01 12:24:14 +0900

    Install gatsby-remark-external-links: 外部サイトは新タブで開く

--- .gatsby/gatsby-config.ts
+++ .gatsby/gatsby-config.ts
@@ -44,7 +44,7 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [`gatsby-remark-prismjs`],
+        plugins: [`gatsby-remark-prismjs`, `gatsby-remark-external-links`],
       },
     },
     {
