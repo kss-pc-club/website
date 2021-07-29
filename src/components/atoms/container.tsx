f49d6d0738a85831536f782e1b71aa58458e8a4b
Ryoga-exe <ryoga.dev@gmail.com>
2021-07-29 15:16:22 +0900

    Create container.tsx

---
+++ src/components/atoms/container.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div<{
+  maxWidth?: string;
+  margin?: string;
+  padding?: string;
+  align?: string;
+}>`
+  margin: ${(props) => props.margin || '0 auto'};
+  padding: ${(props) => props.padding || '0 4vw'};
+  max-width: ${(props) => props.maxWidth || '1120px'};
+  text-align: ${(props) => props.align || 'unset'};
+`;
+
+export default Container;
