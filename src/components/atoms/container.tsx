e3476c836c1411008f12fbd3ed4993e689f98122
Ryoga-exe <ryoga.dev@gmail.com>
2021-07-29 15:17:43 +0900

    Format container.tsx

--- src/components/atoms/container.tsx
+++ src/components/atoms/container.tsx
@@ -1,15 +1,15 @@
-import styled from '@emotion/styled';
+import styled from '@emotion/styled'
 
 const Container = styled.div<{
-  maxWidth?: string;
-  margin?: string;
-  padding?: string;
-  align?: string;
+  maxWidth?: string
+  margin?: string
+  padding?: string
+  align?: string
 }>`
   margin: ${(props) => props.margin || '0 auto'};
   padding: ${(props) => props.padding || '0 4vw'};
   max-width: ${(props) => props.maxWidth || '1120px'};
   text-align: ${(props) => props.align || 'unset'};
-`;
+`
 
-export default Container;
+export default Container
