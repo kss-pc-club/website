20b87e596f34ea57ae1c1dc6586f2368173aec6a
Ryoga.exe <ryoga.dev@gmail.com>
2022-04-30 23:55:47 +0900

    update gatsby-node.ts: slugを自動生成するように

---
+++ gatsby-node.ts
@@ -0,0 +1,26 @@
+import { GatsbyNode } from 'gatsby'
+import { createFilePath } from 'gatsby-source-filesystem'
+
+export const onCreateNode: GatsbyNode['onCreateNode'] = async ({
+  node,
+  actions: { createNodeField },
+  getNode,
+}) => {
+  if (node.internal.type !== `MarkdownRemark`) {
+    return
+  }
+  const [slug, title, date, last_update, description, excerpt] = [
+    node.frontmatter.slug || `/articles` + createFilePath({ node, getNode }),
+    node.frontmatter.title,
+    node.frontmatter.date,
+    node.frontmatter.last_update,
+    node.frontmatter.description,
+    node.excerpt,
+  ]
+  createNodeField({ name: `slug`, node, value: slug })
+  createNodeField({ name: `title`, node, value: title })
+  createNodeField({ name: `date`, node, value: date })
+  createNodeField({ name: `last_update`, node, value: last_update })
+  createNodeField({ name: `description`, node, value: description })
+  createNodeField({ name: `excerpt`, node, value: excerpt })
+}
