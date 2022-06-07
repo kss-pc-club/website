266de9402b48b1b49234c4dce56f2fceb51223bf
eno1220 <enogen.qpt@gmail.com>
2022-06-07 19:01:36 +0900

    Update Container要素に修正

--- src/pages/{MarkdownRemark.fields__slug}.tsx
+++ src/pages/{MarkdownRemark.fields__slug}.tsx
@@ -1,6 +1,6 @@
 import { graphql, Link } from 'gatsby'
 import React from 'react'
-import { Breadcrumb } from 'react-bootstrap'
+import { Breadcrumb, Container } from 'react-bootstrap'
 
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 import 'prismjs/themes/prism.css'
@@ -16,38 +16,40 @@ const MarkdownArticle_template: React.FC<any> = ({ location, data }) => {
     <React.Fragment>
       <Layout location={location} styles={Styles.main}>
         <SEO title={fields.title} description={fields.description} />
-        <Breadcrumb>
-          <Breadcrumb.Item href='/'>トップページ</Breadcrumb.Item>
-          <Breadcrumb.Item href='/articles/'>投稿記事一覧</Breadcrumb.Item>
-          <Breadcrumb.Item active>この記事</Breadcrumb.Item>
-        </Breadcrumb>
-        <h1>{fields.title}</h1>
-        <p id={Styles.date}>
-          <span className={Styles.post_date_tag}>公開日</span>
-          <span className={Styles.post_date}>{fields.date}</span>
-          <br />
-          <span className={Styles.post_date_tag}>更新日</span>
-          <span className={Styles.post_date}>{fields.last_update}</span>
-        </p>
-        <p id={Styles.description}>{fields.description}</p>
-        <hr />
-        <div
-          className={Styles.article}
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-        <hr />
-        <Link
-          to='/'
-          className={`btn btn-outline-secondary ${Styles.footer_btn}`}
-        >
-          トップページへ
-        </Link>
-        <Link
-          to='/articles/'
-          className={`btn btn-outline-secondary ${Styles.footer_btn}`}
-        >
-          記事一覧ページへ
-        </Link>
+        <Container>
+          <Breadcrumb>
+            <Breadcrumb.Item href='/'>トップページ</Breadcrumb.Item>
+            <Breadcrumb.Item href='/articles/'>投稿記事一覧</Breadcrumb.Item>
+            <Breadcrumb.Item active>この記事</Breadcrumb.Item>
+          </Breadcrumb>
+          <h1>{fields.title}</h1>
+          <p id={Styles.date}>
+            <span className={Styles.post_date_tag}>公開日</span>
+            <span className={Styles.post_date}>{fields.date}</span>
+            <br />
+            <span className={Styles.post_date_tag}>更新日</span>
+            <span className={Styles.post_date}>{fields.last_update}</span>
+          </p>
+          <p id={Styles.description}>{fields.description}</p>
+          <hr />
+          <div
+            className={Styles.article}
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+          <hr />
+          <Link
+            to='/'
+            className={`btn btn-outline-secondary ${Styles.footer_btn}`}
+          >
+            トップページへ
+          </Link>
+          <Link
+            to='/articles/'
+            className={`btn btn-outline-secondary ${Styles.footer_btn}`}
+          >
+            記事一覧ページへ
+          </Link>
+        </Container>
       </Layout>
     </React.Fragment>
   )
