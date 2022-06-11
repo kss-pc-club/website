33d93216f09426dd2628206a2a619cb9c0d176a7
Ryoga.exe <ryoga.dev@gmail.com>
2022-06-11 14:23:40 +0900

    update: TopPage

--- src/components/organisms/TopPage/contact.tsx
+++ src/components/organisms/TopPage/contact.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { css } from '@emotion/react'
 import Container from 'src/components/atoms/container'
 import SectionTitle from 'src/components/atoms/section-title'
 
@@ -8,6 +9,18 @@ const Content: React.FC = () => {
     <section id='contact'>
       <Container padding='120px 4vw' align='center'>
         <SectionTitle subTitle='お問い合わせ'>Contact</SectionTitle>
+        <div
+          css={css`
+            width: 95%;
+            margin: auto;
+            text-align: center;
+          `}
+        >
+          <p>
+            部長や顧問の先生に直接お尋ねください。
+            部活動の時間にご自由にお越しいただいて構いません。
+          </p>
+        </div>
       </Container>
     </section>
   )
